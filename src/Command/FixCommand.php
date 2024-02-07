e00b80f36a7566e37b9b0579b907b7b896f77349
jfsenechal <jfsenechal@gmail.com>
2024-02-07 21:55:57 +0100

    fix roles

---
+++ src/Command/FixCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AcMarche\Duobac\Command;
+
+use AcMarche\Duobac\Repository\UserRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'duobac:fix',
+    description: 'Mise à jour des relevés',
+)]
+class FixCommand extends Command
+{
+    private array $types = ['duobac', 'moyenne'];
+
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        foreach ($this->userRepository->findAll() as $user) {
+            $user->roles = ['ROLE_DUOBAC'];
+        }
+
+        $this->userRepository->flush();
+
+        return Command::SUCCESS;
+    }
+}
